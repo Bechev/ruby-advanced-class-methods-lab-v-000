f85af3d7b6168ba540821a96ac55130bf72c465a
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-10 15:31:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,15 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.detect{|song| song.name==name }
+    self.all.detect{ |song| song.name==name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name
+    end
   end
 
 end
